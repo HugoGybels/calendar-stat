315eedd36db815e3f23cb2897c4c40d322cd5050
HUGO GYBELS <@enedis.fr>
2018-12-28 17:28:58 +0100

    Service add-c provider

--- front-angular/src/app/components/add-c/add-c.component.ts
+++ front-angular/src/app/components/add-c/add-c.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {CalendarStatService} from "../../services/calendar-stat/calendar-stat.service";
 
 @Component({
   selector: 'app-add-c',
@@ -7,12 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddCComponent implements OnInit {
 
-  constructor() { }
+  constructor(private calendarStatService: CalendarStatService) { }
 
   ngOnInit() {
   }
 
   addC() {
+    this.calendarStatService.addC(new Date());
     console.log('add-c');
   }
 
